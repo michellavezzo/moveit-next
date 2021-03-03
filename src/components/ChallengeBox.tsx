0f2618f367a124c8d84fe601a0612eb42105b716
Michel Lavezzo <michellavezzo@users.noreply.github.com>
2021-03-03 16:56:51 -0300

    challenges Contexts integrated to components

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -3,27 +3,25 @@ import { challengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox(){
-    const contextData = useContext(challengesContext);    
-    const hasActiveChallenge = true;
-
-    console.log(contextData);
+    const { activeChallenge, resetChallenge } = useContext(challengesContext);  
 
     return(
         <div className={styles.challengeBoxContainer}>
-            { hasActiveChallenge ? (
+            { activeChallenge ? (
                 <div className={styles.challengeActive} > 
-                    <header> Ganhe 400 xp</header>
+                    <header> Ganhe {activeChallenge.amount} xp</header>
 
                     <main>
-                        <img src="icons/body.svg"  />   
+                        <img src={`icons/${activeChallenge.type}.svg`}  />   
                         <strong> Novo desafio </strong>
-                        <p>Levante e faça uma caminhada de 3 minutos</p>
+                        <p>{activeChallenge.description}</p>
                     </main>
 
                     <footer>
                         <button  
                         type="button"
-                        className={styles.challengeFailedButton}    
+                        className={styles.challengeFailedButton}   
+                        onClick={ resetChallenge }
                         >
                             Falhei
                         </button>
@@ -51,5 +49,5 @@ export function ChallengeBox(){
     )
 }
 
-// 28:00
+// 59:00 aula 3
 //https://www.youtube.com/watch?v=ArqCBqN7zzc&feature=emb_title
